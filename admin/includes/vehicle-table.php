06b0b3ed658139c397622fa1503e2115d8c4a68f
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2021-05-19 19:57:30 +0100

    Bulk of Vehicle table complete

--- admin/includes/vehicle-table.php
+++ admin/includes/vehicle-table.php
@@ -15,21 +15,25 @@ require('../includes/connect_db.php');
                 <thead>
                     <tr>
                         <th>Vehicle ID</th>
-                        <th>Campus ID</th>
+                        <th>Vehicle Registration</th>
+                        <th>Assigned Campus</th>
                         <th>Vehicle Make</th>
                         <th>Vehicle Model</th>
                         <th>Vehicle Colour</th>
                         <th>Status</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tfoot>
                     <tr>
                         <th>Vehicle ID</th>
-                        <th>Campus ID</th>
+                        <th>Vehicle Registration</th>
+                        <th>Assigned Campus</th>
                         <th>Vehicle Make</th>
                         <th>Vehicle Model</th>
                         <th>Vehicle Colour</th>
                         <th>Status</th>
+                        <th>Actions</th>
                     </tr>
                 </tfoot>
                 <tbody>
@@ -42,22 +46,28 @@ require('../includes/connect_db.php');
                     ?>
                             <tr>
                                 <td><?php echo "{$row['vehicle_id']}"; ?></td>
-                                <td><?php echo "{$row['campus_id']}"; ?></td>
+                                <td><?php echo "{$row['vehicle_reg']}"; ?></td>
+                                <td><?php       
+                                
+                                $cam = $coc['campus_id'];
+                                $cok = mysqli_query($link, "SELECT campus_name FROM campus WHERE campus_id = '$cam'");
+
+                                echo "{$row['campus_id']}"; ?></td>
                                 <td><?php echo "{$row['vehicle_make']}"; ?></td>
                                 <td><?php echo "{$row['vehicle_model']}"; ?></td>
                                 <td><?php echo "{$row['vehicle_colour']}"; ?></td>
-                                <td><?php echo "{$row['status']}"; ?></td>
+                                <td><?php echo "{$row['vehicle_status']}"; ?></td>
                                 <td>
                                     <div class="row">
                                         <a href="#editvehicleModal<?php echo "{$row['vehicle_id']}"; ?>" data-toggle="modal" style="text-decoration:none; color:inherit;"><button class="btn"><em class="fa fa-edit"></em> Edit</a></button>
                                         <form action="includes/delete.php" method="post">
-                                            <input type="hidden" id="vehicleID" name="vehicle_id" value="<?php echo "{$row['tv_id']}"; ?>">
+                                            <input type="hidden" id="vehicleID" name="vehicle_id" value="<?php echo "{$row['vehicle_id']}"; ?>">
                                             <button type="submit" name="btnDeleteUsr" class="btn" value=""><em class="fa fa-trash"></em> Delete</button>
                                         </form>
                                     </div>
                                 </td>
-<!--
-                                <div class="modal fade" id="editvehicleModal<?php echo "{$row['tv_id']}"; ?>" tabindex="-1" role="dialog" aria-labelledby="details" aria-hidden="true">
+
+                                <div class="modal fade" id="editvehicleModal<?php echo "{$row['vehicle_id']}"; ?>" tabindex="-1" role="dialog" aria-labelledby="details" aria-hidden="true">
                                     <div class="modal-dialog modal-dialog-centered" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
@@ -68,52 +78,59 @@ require('../includes/connect_db.php');
                                             </div>
                                             <div class="modal-body">
                                                 <form action="includes/edit.php" method="post">
-                                                    <input type="hidden" id="tvId" name="tv_id" value="<?php echo "{$row['tv_id']}"; ?>">
+                                                    <input type="hidden" id="vehicleId" name="vehicle_id" value="<?php echo "{$row['vehicle_id']}"; ?>">
                                                     <div class="form-group">
-                                                        <input type="text" name="tv_title" class="form-control" placeholder="<?php echo "{$row['tv_title']}"; ?>" value="<?php if (isset($_POST['tv_title'])) {
-                                                                                                                                                                                echo $_POST['tv_title'];
-                                                                                                                                                                            } ?>">
+                                                        <input type="text" name="vehicle_reg" class="form-control" placeholder="<?php echo "{$row['vehicle_reg']}"; ?>" value="<?php if (isset($_POST['vehicle_reg'])) {
+                                                                                                                                                                                    echo $_POST['vehicle_reg'];
+                                                                                                                                                                                } ?>">
 
                                                     </div>
                                                     <div class="form-group">
-                                                        <input type="text" name="tv_desc" class="form-control" placeholder="<?php echo "{$row['tv_desc']}"; ?>" value="<?php if (isset($_POST['tv_desc'])) {
-                                                                                                                                                                            echo $_POST['tv_desc'];
-                                                                                                                                                                        } ?>">
-
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <select name="tv_genre" class="form-control">
-                                                            <option value="" selected disabled hidden>Change Genre</option>
+                                                        <select name="campus_id" class="form-control">
+                                                            <option value="" selected disabled hidden>Change Assigned Campus</option>
 
                                                             <?php
-                                                            /*
-                                                            $f = "SELECT * FROM genre";
+
+                                                            $f = "SELECT * FROM campus";
                                                             $a = mysqli_query($link, $f);
                                                             if (mysqli_num_rows($a) > 0) {
                                                                 while ($coc = mysqli_fetch_array($a, MYSQLI_ASSOC)) {
 
                                                             ?>
-                                                                    <option value="<?php echo "{$coc['genre_name']}"; ?>"><?php echo "{$coc['genre_name']}"; ?></option>
+                                                                    <option value="<?php echo "{$coc['campus_id']}"; ?>"><?php echo "{$coc['campus_name']}"; ?></option>
                                                             <?php
                                                                 }
                                                             }
-                                                            */
+
                                                             ?>
 
                                                         </select>
 
                                                     </div>
                                                     <div class="form-group">
-                                                        <input type="url" name="tv_img" class="form-control" placeholder="<?php echo "{$row['tv_img']}"; ?>" value="<?php if (isset($_POST['tv_img'])) {
-                                                                                                                                                                        echo $_POST['tv_img'];
-                                                                                                                                                                    } ?>">
+                                                        <input type="text" name="vehicle_make" class="form-control" placeholder="<?php echo "{$row['vehicle_make']}"; ?>" value="<?php if (isset($_POST['vehicle_make'])) {
+                                                                                                                                                                                    echo $_POST['vehicle_make'];
+                                                                                                                                                                                } ?>">
 
                                                     </div>
                                                     <div class="form-group">
-                                                        <input type="url" name="tv_trailer" class="form-control" placeholder="<?php echo "{$row['tv_trailer']}"; ?>" value="<?php if (isset($_POST['tv_trailer'])) {
-                                                                                                                                                                                echo $_POST['tv_trailer'];
-                                                                                                                                                                            } ?>">
+                                                        <input type="text" name="vehicle_model" class="form-control" placeholder="<?php echo "{$row['vehicle_model']}"; ?>" value="<?php if (isset($_POST['vehicle_model'])) {
+                                                                                                                                                                                        echo $_POST['vehicle_model'];
+                                                                                                                                                                                    } ?>">
 
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <input type="text" name="vehicle_colour" class="form-control" placeholder="<?php echo "{$row['vehicle_colour']}"; ?>" value="<?php if (isset($_POST['vehicle_colour'])) {
+                                                                                                                                                                                        echo $_POST['vehicle_colour'];
+                                                                                                                                                                                    } ?>">
+
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <select name="vehicle_status" class="form-control">
+                                                            <option value="" selected disabled hidden>Vehicle Status</option>
+                                                            <option value="1">Inactive</option>
+                                                            <option value="2">Active</option>
+                                                        </select>
                                                     </div>
                                                     <div class="modal-footer">
                                                         <div class="form-group">
@@ -125,7 +142,6 @@ require('../includes/connect_db.php');
                                         </div>
                                     </div>
                                 </div>
-                                -->
 
                             </tr>
                     <?php
@@ -141,12 +157,12 @@ require('../includes/connect_db.php');
     </div>
 </div>
 
-<!--
+
 <div class="modal fade" id="addvehicleModal" tabindex="-1" role="dialog" aria-labelledby="details" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Change Details</h5>
+                <h5 class="modal-title" id="exampleModalCenterTitle">Add Vehicle</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -154,50 +170,57 @@ require('../includes/connect_db.php');
             <div class="modal-body">
                 <form action="includes/edit.php" method="post">
                     <div class="form-group">
-                        <input type="text" name="new_tv" class="form-control" placeholder="vehicle Name" value="<?php if (isset($_POST['new_tv'])) {
-                                                                                                                    echo $_POST['new_tv'];
-                                                                                                                } ?>" required>
+                        <input type="text" name="new_vehicle" class="form-control" placeholder="Vehicle Registration" value="<?php if (isset($_POST['new_vehicle'])) {
+                                                                                                                                    echo $_POST['new_vehicle'];
+                                                                                                                                } ?>" required>
 
                     </div>
                     <div class="form-group">
-                        <input type="text" name="tv_desc" class="form-control" placeholder="vehicle Description" value="<?php if (isset($_POST['tv_desc'])) {
-                                                                                                                            echo $_POST['tv_desc'];
-                                                                                                                        } ?>" required>
-
-                    </div>
-                    <div class="form-group">
-                        <select name="tv_genre" class="form-control" required>
-                            <option value="" selected disabled hidden>Please Select Genre</option>
+                        <select name="campus_id" class="form-control">
+                            <option value="" selected disabled hidden>Change Assigned Campus</option>
 
                             <?php
-                            /*
-                            $f = "SELECT * FROM genre";
+
+                            $f = "SELECT * FROM campus";
                             $a = mysqli_query($link, $f);
                             if (mysqli_num_rows($a) > 0) {
                                 while ($coc = mysqli_fetch_array($a, MYSQLI_ASSOC)) {
 
                             ?>
-                                    <option value="<?php echo "{$coc['genre_name']}"; ?>"><?php echo "{$coc['genre_name']}"; ?></option>
+                                    <option value="<?php echo "{$coc['campus_id']}"; ?>"><?php echo "{$coc['campus_name']}"; ?></option>
                             <?php
                                 }
                             }
-                            */
+
                             ?>
 
                         </select>
 
                     </div>
                     <div class="form-group">
-                        <input type="url" name="tv_img" class="form-control" placeholder="Image URL" value="<?php if (isset($_POST['mov_img'])) {
-                                                                                                                echo $_POST['mov_img'];
-                                                                                                            } ?>" required>
+                        <input type="text" name="vehicle_make" class="form-control" placeholder="Vehicle Make" value="<?php if (isset($_POST['vehicle_make'])) {
+                                                                                                                            echo $_POST['vehicle_make'];
+                                                                                                                        } ?>" required>
 
                     </div>
                     <div class="form-group">
-                        <input type="url" name="tv_trailer" class="form-control" placeholder="Trailer URL" value="<?php if (isset($_POST['mov_trailer'])) {
-                                                                                                                        echo $_POST['mov_trailer'];
-                                                                                                                    } ?>" required>
+                        <input type="text" name="vehicle_model" class="form-control" placeholder="Vehicle Model" value="<?php if (isset($_POST['vehicle_model'])) {
+                                                                                                                            echo $_POST['vehicle_model'];
+                                                                                                                        } ?>" required>
 
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="vehicle_colour" class="form-control" placeholder="Vehicle Colour" value="<?php if (isset($_POST['vehicle_colour'])) {
+                                                                                                                                echo $_POST['vehicle_colour'];
+                                                                                                                            } ?>" required>
+
+                    </div>
+                    <div class="form-group">
+                        <select name="vehicle_status" class="form-control">
+                            <option value="" selected disabled hidden>Vehicle Status</option>
+                            <option value="1">Inactive</option>
+                            <option value="2">Active</option>
+                        </select>
                     </div>
                     <div class="modal-footer">
                         <div class="form-group">
@@ -209,4 +232,3 @@ require('../includes/connect_db.php');
         </div>
     </div>
 </div>
--->
