0ebe5df373a28e61ba323cdfe7640c9a8d7475ef
Dougie1902 <Proxxide163@gmail.com>
2021-05-25 15:43:20 +0100

    Scroll fix

--- index.php
+++ index.php
@@ -49,36 +49,34 @@ require('includes/connect_db.php');
 
 	<!--News Cards-->
 <div class="d-flex justify-content-center">
-  <div class="row d-flex justify-content-center">
+  <div class="row d-flex justify-content-center mx-auto">
 
     <?php
-    $q = "SELECT * FROM news";
+    $q = "SELECT * FROM news order by post_date  desc LIMIT 6";
     $r = mysqli_query($link, $q);
     if (mysqli_num_rows($r) > 0) {
-
-      while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-        $short = substr($row['post_content'], 0, 197) . "...";  // returns "abcde"
-    ?>
-        <div class="card" style="width: 33rem; margin:1rem;">
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title"><?php echo "{$row['post_title']}"; ?></h5>
-            <h6 class="card-subtitle mb-2 text-muted"><?php echo "{$row['post_date']}"; ?></h6>
-            <p class="card-text"><?php echo "$short"; ?></p>
-            <div class="card-footer bg-transparent mt-auto">
-              <a href="article.php?id=<?php echo "{$row['post_id']}"; ?>" class="card-link">Read Full Article</a>
+        while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+          $short = substr($row['post_content'], 0, 197) . "...";  // returns "abcde"
+      ?>
+          <div class="card" style="width: 33rem; margin:1rem;">
+            <div class="card-body d-flex flex-column">
+              <h5 class="card-title"><?php echo "{$row['post_title']}"; ?></h5>
+              <h6 class="card-subtitle mb-2 text-muted"><?php echo "{$row['post_date']}"; ?></h6>
+              <p class="card-text"><?php echo "$short"; ?></p>
+              <div class="card-footer bg-transparent mt-auto">
+                <a href="article.php?id=<?php echo "{$row['post_id']}"; ?>" class="card-link">Read Full Article</a>
+              </div>
             </div>
           </div>
-        </div>
-
-    <?php
-      }
-    }
-    # Or display message.
-    else {
-      echo '<p>There are currently no news posts</p>';
-    }
+  
+          <?php
+          }
+        }
+      # Or display message.
+      else {
+        echo '<p>There are currently no news posts</p>';
+      }   
     ?>
-
   </div>
 </div>
 
